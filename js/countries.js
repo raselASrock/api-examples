cba3a31099bcb40ac20dd747629e2da51f334fd8
raselASrock <mdraselm413@gmail.com>
2022-08-28 23:58:41 +0600

    last

--- js/countries.js
+++ js/countries.js
@@ -1,1 +1,13 @@
-console.log('countries added')
+const loadCountries = () => {
+    fetch('https://restcountries.com/v3.1/all')
+    .then( response => response.json())
+    .then( data => displayCountries(data))
+}
+
+const displayCountries = countries => {
+    for( const country of countries){
+        console.log(country)
+    }
+}
+
+loadCountries();
