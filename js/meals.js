262962e728ef1d96db60ead890e9e7a9c2dfd435
raselASrock <mdraselm413@gmail.com>
2022-08-29 15:45:58 +0600

    meals-added

---
+++ js/meals.js
@@ -0,0 +1,14 @@
+const loadMeals = () => {
+    fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a')
+    .then( res => res.json())
+    .then( data => displayMeals(data.meals))
+}
+
+const displayMeals = meals => {
+    const mealContainer = document.getElementById('meal-container')
+    meals.forEach(meal =>{
+        console.log(meal)
+    })
+}
+
+loadMeals()
