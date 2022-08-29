f4fcf3e947aabf6a9d46d495c08aff23c011c7c5
raselASrock <mdraselm413@gmail.com>
2022-08-29 19:37:54 +0600

    food-added

--- js/meals.js
+++ js/meals.js
@@ -1,14 +1,35 @@
-const loadMeals = () => {
-    fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a')
-    .then( res => res.json())
-    .then( data => displayMeals(data.meals))
+const loadMeals = (search) => {
+    const URL = `https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`
+    fetch(URL)
+        .then(res => res.json())
+        .then(data => displayMeals(data.meals))
 }
 
 const displayMeals = meals => {
     const mealContainer = document.getElementById('meal-container')
-    meals.forEach(meal =>{
+    mealContainer.innerHTML = ''
+    meals.forEach(meal => {
         console.log(meal)
+        const mealDiv = document.createElement('div')
+        mealDiv.classList.add('col')
+        mealDiv.innerHTML = `
+        <div class="card">
+            <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${meal.strMeal}</h5>
+                <p class="card-text">${meal.strInstructions.slice(0,200)}</p>
+            </div>
+        </div>
+        `
+        mealContainer.appendChild(mealDiv)
     })
 }
 
-loadMeals()
+const searchFood = () => {
+    const searchField = document.getElementById('search-field')
+    const searchText = searchField.value 
+    loadMeals(searchText)
+    searchField.value = ''
+}
+
+// loadMeals('a')
