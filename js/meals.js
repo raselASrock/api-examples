4196b3c9de1922c5d512c0cd859501bfd4fd4a3c
raselASrock <mdraselm413@gmail.com>
2022-08-29 22:40:43 +0600

    new-code

--- js/meals.js
+++ js/meals.js
@@ -13,7 +13,7 @@ const displayMeals = meals => {
         const mealDiv = document.createElement('div')
         mealDiv.classList.add('col')
         mealDiv.innerHTML = `
-        <div onclick="displayMealDetail(${meal.idMeal})" class="card">
+        <div onclick="loadMealDetail(${meal.idMeal})" class="card">
             <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">${meal.strMeal}</h5>
@@ -33,9 +33,20 @@ const searchFood = () => {
 }
 
 
-const displayMealDetail = (idMeal) => {
+const loadMealDetail = (idMeal) => {
     // console.log('Get details of id', idMeal)
-    const url = `www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
-    console.log(url)
+    const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`;
+    // console.log(url)
+
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayMealDetails(data.meals[0]));
 }
+
+const displayMealDetails = meal => {
+    const detailContainer = document.getElementById('detail-container');
+    const mealDiv = document.createElement('div')
+}
+
+
 loadMeals('')
